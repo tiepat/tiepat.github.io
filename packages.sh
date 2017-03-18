c9213a9af6801ba69c10ba6cc09a2b9eaa215d37
Tiepat <anounomus455@gmail.com>
2017-03-18 11:45:10 -0700

    Update

--- packages.sh
+++ packages.sh
@@ -6,6 +6,7 @@ find ./projects -type f -name '*.DS_STORE' -delete
 #find ./projects/<PROJECT NAME>/Library -type f -name '*.DS_STORE' -delete
 #find ./projects/<PROJECT NAME>/Library/Themes -type f -name '*.DS_STORE' -delete
 #find ./projects/<PROJECT NAME>/Library/Themes/<FOLDER NAME>.theme -type f -name '*.DS_STORE' -delete
+#find ./projects/<PROJECT NAME>/Library/Themes/<FOLDER NAME>.theme/Bundles -type f -name '*.DS_STORE' -delete
 #dpkg-deb -bZgzip projects/<PROJECT NAME> debs
 
 find ./projects/TwistedApple/DEBIAN -type f -name '*.DS_STORE' -delete
@@ -14,4 +15,7 @@ find ./projects/TwistedApple/Library/Themes -type f -name '*.DS_STORE' -delete
 find ./projects/TwistedApple/Library/Themes/TwistedApple-ControlCenter.theme -type f -name '*.DS_STORE' -delete
 find ./projects/TwistedApple/Library/Themes/TwistedApple-Icons.theme -type f -name '*.DS_STORE' -delete
 find ./projects/TwistedApple/Library/Themes/TwistedApple-RespringLogo.theme -type f -name '*.DS_STORE' -delete
+find ./projects/TwistedApple/Library/Themes/TwistedApple-ControlCenter.theme/Bundles -type f -name '*.DS_STORE' -delete
+find ./projects/TwistedApple/Library/Themes/TwistedApple-Icons.theme/Bundles -type f -name '*.DS_STORE' -delete
+find ./projects/TwistedApple/Library/Themes/TwistedApple-RespringLogo.theme/Bundles -type f -name '*.DS_STORE' -delete
 dpkg-deb -bZgzip projects/TwistedApple debs
