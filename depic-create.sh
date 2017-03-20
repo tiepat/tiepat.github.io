c6b3da28336e32116a54934d60435cfed2138569
Tiepat <anounomus455@gmail.com>
2017-03-20 07:22:22 -0700

    Update Depics

--- depic-create.sh
+++ depic-create.sh
@@ -8,11 +8,8 @@ read varcat
 echo Thanks! Creating...
 mkdir ./depic/$varname
 cat <<EOT >> ./depic/$varname/index.html
-<h1 style="text-align: center;"><span style="color: #ffffff; background-color: #339966;"><img src="http://tiepat.github.io/CydiaIcon.png" alt="" width="22" height="22" />&nbsp;tiepat's Repository &nbsp;</span></h1>
+<h1 style="text-align: center;"><span style="color: #ffffff; background-color: #339966;">&nbsp; tiepat's Repository &nbsp;</span></h1>
 <h3 style="text-align: center;">$varname - $varcat</h3>
-<p style="text-align: center;">&nbsp;</p>
-<p style="text-align: center;">&nbsp;</p>
-<p style="text-align: center;">&nbsp;</p>
 <p style="text-align: center;">Description: $vardesc</p>
 <p style="text-align: center;">Compatability: IOS 10.0 - 10.2</p>
 <p style="text-align: center;">Screenshots:</p>
