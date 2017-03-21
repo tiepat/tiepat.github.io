7b0ff7a9325380815269e6fa03a35f18f3f3479d
Tiepat <anounomus455@gmail.com>
2017-03-20 20:58:12 -0700

    update

--- depic/TwistedApple/index.sh
+++ depic/TwistedApple/index.sh
@@ -4,4 +4,4 @@ name = "TwistedApple"
 desc = "A minimalistic theme that custimizes every aspect of your phone."
 cat = "Theme"
 change = "1-1.6 : Added and perfected system icons, some control center glyphs, and the respring logo."
-echo "$code" >> /depic/TwistedApple/index.html
+echo $code >> /depic/TwistedApple/index.html
