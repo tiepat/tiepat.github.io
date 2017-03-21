138398c103c6b007b9c6ade4c27f249db6603d88
Tiepat <anounomus455@gmail.com>
2017-03-21 07:16:23 -0700

    update

--- depic/index.sh
+++ depic/index.sh
@@ -1,7 +1,7 @@
 find ./TwistedApple/ -type f -name '*.html' -delete
+varname = "TwistedApple"
+vardesc = "A minimalistic theme that custimizes every aspect of your phone."
+varcat = "Theme")
+varchange = "1-1.6 : Added and perfected system icons, some control center glyphs, and the respring logo."
 var4="$(echo ./template.txt | cut -f1 -d '.')"
-varname = $("TwistedApple")
-vardesc = $("A minimalistic theme that custimizes every aspect of your phone.")
-varcat = $("Theme")
-varchange = $("1-1.6 : Added and perfected system icons, some control center glyphs, and the respring logo.")
 echo "$var4" >> ./TwistedApple/index.html
